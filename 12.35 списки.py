39bec4a8d0866da54242bf51fff54e7a948893fa
AndreyKokorin09 <55593978+AndreyKokorin09@users.noreply.github.com>
2019-11-27 20:39:22 +0700

    Add files via upload
    12.36a

--- 12.35 списки.py
+++ 12.35 списки.py
@@ -1,6 +1,8 @@
-a=[[0,2,4,6], [1,5,9,13],[3,10,17,19]]
-for i in range (1):
+a=[[0, 2,  4,   6],
+     [1, 5,   9,  13],
+     [3, 10, 17, 19]]
+for j in range( 1,2):
     s=0
-    for j in range (4):
+    for i in range( 3):
         s+=a[i][j]
     print(s)
